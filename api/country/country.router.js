ca4c2c5ff851400aaf0adbd166f4992750924344
Mahesh10 Patil <Mahesh10.Patil@ril.com>
2022-02-10 18:44:54 +0530

    [TJMANDI] added custom validation in rest api.

--- api/country/country.router.js
+++ api/country/country.router.js
@@ -11,23 +11,29 @@ router.get('/countries', getCountry);
 
 router.get('/country/id', [
     check('startYear').custom((value, { req }) => {
-        if (value.length != 4) {
+        if(!value) {
+            throw new Error('Start Year parameter missing.');
+        }
+        else if (value.length != 4) {
           throw new Error('Enter valid Start Year');
         }
         else if(value >= req.query.endYear) {
             throw new Error('Start Year should be less than End Year');
         }
-        // Indicates the success of this synchronous custom validator
+        
         return true;
       }),
 check('endYear').custom((value, { req }) => {
-    if (value.length != 4) {
+    if(!value) {
+        throw new Error('End Year parameter missing.');
+    }
+    else if (value.length != 4) {
       throw new Error('Enter valid End Year');
     }
     else if(value <= req.query.startYear) {
         throw new Error('End Year should be greter than Start Year');
     }
-    // Indicates the success of this synchronous custom validator
+    
     return true;
   }),
 check('category').not().isEmpty().withMessage("Category cannot be empty").trim()
